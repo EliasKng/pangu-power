fefe00c7e4b077a87eabed4bbd57222d4e095db4
EliasKng <elias.koenig@student.kit.edu>
2024-11-21 16:15:39 +0100

    Add RMSE func

--- models/test_power.py
+++ models/test_power.py
@@ -7,7 +7,7 @@ from wind_fusion.pangu_pytorch.models.train_power import (
     load_land_sea_mask,
     visualize,
 )
-from era5_data import utils_data
+from era5_data import utils_data, score
 
 
 warnings.filterwarnings(
@@ -22,7 +22,11 @@ warnings.filterwarnings(
 
 
 def test(test_loader, model, device, res_path):
+    rmse_power = dict()
+    # acc_power = dict()
+
     aux_constants = utils_data.loadAllConstants(device=device)
+
     for id, data in enumerate(test_loader, 0):
         timestamp = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
         print(f"[{timestamp}] predict on {id}")
@@ -41,11 +45,19 @@ def test(test_loader, model, device, res_path):
             target_power_test.to(device),
         )
         model.eval()
+
+        # Inference
         output_power_test, output_surface_test = model_inference(
             model, input_test, input_surface_test, aux_constants
         )
+
+        # Apply lsm
         lsm_expanded = load_land_sea_mask(output_power_test.device)
+        print("Make sure LSM contains NAs and not 0s")
+        # TODO(EliasKng): Check if this is correct
         output_power_test = output_power_test * lsm_expanded
+
+        # Visualize
         target_time = periods_test[1][0]
         png_path = os.path.join(res_path, "png")
         utils.mkdirs(png_path)
@@ -58,3 +70,11 @@ def test(test_loader, model, device, res_path):
             target_time,
             png_path,
         )
+
+        # Compute test scores
+        # RMSE
+        output_power_test = output_power_test.squeeze()
+        target_power_test = target_power_test.squeeze()
+
+        rmse_power[target_time] = score.rmse(output_power_test, target_power_test)
+        # TODO(EliasKng): Implement score calculation
