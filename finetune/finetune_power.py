41a6d8084803f4fe289bd1731322eb39fa22fc76
EliasKng <elias.koenig@student.kit.edu>
2025-01-03 09:27:37 +0100

    DA_1_Lo_Test6

--- finetune/finetune_power.py
+++ finetune/finetune_power.py
@@ -403,7 +403,7 @@ def test_baselines(args, baseline_type):
 
 if __name__ == "__main__":
     models_to_train_or_test = [
-        "DA_1_Lo_Test7",
+        "DA_1_Lo_Test6",
     ]
 
     for type_net in models_to_train_or_test:
